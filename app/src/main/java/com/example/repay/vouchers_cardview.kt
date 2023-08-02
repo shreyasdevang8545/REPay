1a1a519b628bb1294eb082a604297dc3659e2401
EAX2KOR <r.neha@in.bosch.com>
2023-08-02 12:10:08 +0530

    neha's commit

--- app/src/main/java/com/example/repay/vouchers_cardview.kt
+++ app/src/main/java/com/example/repay/vouchers_cardview.kt
@@ -3,7 +3,7 @@ package com.example.repay
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class vouchers_cardview : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vouchers_cardview)
