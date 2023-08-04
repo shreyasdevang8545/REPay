5cf3689a73ab911c596f537059492bd3392f6364
EAX2KOR <r.neha@in.bosch.com>
2023-08-04 11:26:41 +0530

    sdp and ssp changes 2

--- app/src/main/java/com/example/repay/MainActivity.kt
+++ app/src/main/java/com/example/repay/MainActivity.kt
@@ -1,26 +1,8 @@
 package com.example.repay
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.example.repay.databinding.FragmentOtpRequestMainBinding
-import com.example.repay.databinding.ToolbarBinding
+
 
 class MainActivity : AppCompatActivity() {
-    private var mainBinding: FragmentOtpRequestMainBinding?=null
-    private var toolbarBinding: ToolbarBinding?=null
 
-    var requestOtpFragment = RequestOtpFragment()
-    var myVoucherFragment = MyVoucherQrDisplay()
-    var navigationDetails = NavigationDetails()
-    var mainActivity = MainActivity()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        mainBinding = FragmentOtpRequestMainBinding.inflate(layoutInflater)
-        toolbarBinding = ToolbarBinding.inflate(layoutInflater)
-        setContentView(mainBinding!!.root)
-        val transactionManager = supportFragmentManager.beginTransaction()
-        transactionManager.replace(R.id.fragment_container, requestOtpFragment)
-        transactionManager.commit()
     }
-}
